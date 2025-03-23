e6235432243933ce59d44480f77f03b2c4461cd4
xoruv <iancooper11309@gmail.com>
2025-03-22 22:18:39 -0500

    Update site title to xoru.lol

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,13 +9,13 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextSeo
-        title={"xoru's personal site"}
+        title={"xoru.lol"}
         description={"xoru's personal site"}
         canonical={`https://xoru.lol${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`}
         themeColor={"#2563eb"}
         openGraph={{
           url: `https://xoru.lol${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`,
-          title: "xoru's personal site",
+          title: "xoru.lol",
           description: "xoru's personal site",
           images: [
             {
